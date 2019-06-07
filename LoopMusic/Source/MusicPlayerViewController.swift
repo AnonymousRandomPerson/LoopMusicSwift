b6d370bdaea29ab175340e1b090d08d36fb72098
AnonymousRandomPerson <chenghanngan.us@gmail.com>
2019-06-07 00:04:34 -0400

    Move comments to docstrings

---
+++ LoopMusic/Source/MusicPlayerViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+/// Controller for the music player (home screen) of the app.
+class MusicPlayerViewController: UIViewController {
+
+    /// Do any additional setup after loading the view.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
